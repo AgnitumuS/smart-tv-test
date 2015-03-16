15ea6b833ebfac60e33cf89d5bce6cf21c4adf24
Vitalii Sashyn <v.sashyn@lanet.ua>
2015-03-16 17:33:41 +0200

    lgwebos: add flex design, video element, control buttons

--- js/test.js
+++ js/test.js
@@ -1,5 +1,7 @@
-//var XMLHttpRequest = require('xhr2')
-var apiURL = "http://tvapi.la.net.ua/";
+
+var $apiURL = "http://tvapi.la.net.ua/";
+var $staticURL = "http://static.la.net.ua/";
+var $edge;
 
 function handler(){
 	var response = JSON.parse(this.responseText);
@@ -8,8 +10,6 @@ function handler(){
 	for(i =0 ; i<response.list.length; i++){
 		inner += response.list[i]._id + "<br>";
 	}
-	window.document.getElementById("test").innerHTML = inner;
-
 	//url(//static.lanet.ua/tv/logo/9032.png)
 }
 
@@ -21,19 +21,54 @@ function getTVAPI(url) {
 
 }
 
-var $play = {
-	init : function (){
+// var $play = {
+// 	init : function (){
 
-	},
-	load : function (){
+// 	},
+// 	load : function (){
 
-	}
-}
-document.getElementById("test").style["background-image"] = "http://static.lanet.ua/tv/logo/9032.png";
+// 	}
+// }
+//document.getElementById("test").style["background-image"] = "http://static.lanet.ua/tv/logo/9032.png";
 
+/* start jquery when web page is loaded */
+// function buildChannels(data, status, xhr){
+// 	for( i=0 ; i<data.list.length ; i++){
+// 		$("#flex-main").append('<div class="flex-item"> 1 </div>')
+// 	};
+// }
 
-getTVAPI(apiURL + "list.json");
+//$(document).ready(function(){
+	$.getJSON($apiURL + "list.json",function(data){
+		$edge = data.edge;
+		for( i=0 ; i<data.list.length ; i++){
+		$("#flex-main").append('<div class="flex-item"' + " data-id=\"" + data.list[i]._id  + '\"' 
+			+  'style="background-image: url(\'' +$staticURL + 'tv/logo/'+ data.list[i]._id + '.png\');"'
+			+  '</div>');
+	};})
+	
+//})
+
+//getTVAPI($apiURL + "list.json");
 //Event Listeners
-document.getElementById("test").addEventListener("click", function(){
-	alert("event Listener");
-});
+//
+// $(document).ready(function(){
+// 	var elems = document.getElementsByClassName("flex-item");
+// 	for(i=0; i<elems.length;i++){
+// 		elems[i].addEventListener("click", function(){
+// 			alert("event Listener")
+// 		});
+// }
+// })
+
+
+
+	$("#flex-main").on("click",'.flex-item', function(){
+		
+		//this.innerHTML = "OOOPS";	//debug
+		//alert($(this).attr('data-id'));	//debug
+		var playlist = $edge + $(this).attr('data-id') + '.m3u8';
+		$("#iPlayer").attr("src", playlist);
+		//alert(this);
+	})	
+
