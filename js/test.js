75ae60ab92fb728385a1c891fddf2383773bde8f
v.sashyn <v.sashyn@lanet.ua>
2015-03-25 12:10:10 +0200

    lgottwebos: removed flex

--- js/test.js
+++ js/test.js
@@ -3,30 +3,15 @@ var $apiURL = "http://tvapi.la.net.ua/";
 var $staticURL = "http://static.la.net.ua/";
 var $edge;
 
-function handler(){
-	var response = JSON.parse(this.responseText);
-	var inner;
-	inner = response.list[0]["_id"];
-	for(i =0 ; i<response.list.length; i++){
-		inner += response.list[i]._id + "<br>";
-	}
-	//url(//static.lanet.ua/tv/logo/9032.png)
-}
 
-function getTVAPI(url) {
-	var xmlHttp = new XMLHttpRequest();
-	xmlHttp.open("GET", url, true);
-	xmlHttp.onload = handler;			//callback
-	xmlHttp.send(null);
-
-}
 
 var $play = {
 	init : function (){
-
+		var playlist = 'http://kirito.la.net.ua/tv/9006.m3u8';
+		$("#iPlayer").attr('src', playlist);
 	},
-	load : function (){
-
+	load : function (playlist){
+		$('#iPlayer').attr('src', playlist);
 	}
 }
 
@@ -36,75 +21,49 @@ $(document).ready(function(){
 			$edge = data.edge;
 			for( i=0 ; i<data.list.length ; i++){
 				var tabindex = 1;
-			$(".child").append('<div class="chan" tabindex='+ tabindex++ + " data-id=\"" + data.list[i]._id  + '\"'  
+			$(".left").append('<div class="chan" tabindex='+ tabindex++ + " data-id=\"" + data.list[i]._id  + '\"'  
 				+  'style="background-image: url(\'' +$staticURL + 'tv/logo/'+ data.list[i]._id + '.png\');">' 
 				+ '</div>');
 		};});
 });
 
-//getTVAPI($apiURL + "list.json");
-//
-// $(document).ready(function(){
-// 	var elems = document.getElementsByClassName("flex-item");
-// 	for(i=0; i<elems.length;i++){
-// 		elems[i].addEventListener("click", function(){
-// 			alert("event Listener")
-// 		});
-// }
-// })
+
 
 /*  resize screen	*/
-			// alert($(document).width()) + "\n ";
-			// $("#iPlayer").attr({
-			// 	"width": document.documentElement.clientWidth, 
-			// 	"height":document.documentElement.clientHeight
-			// });
+			 $("#iPlayer").attr({
+			 	"width": document.documentElement.clientWidth, 
+			 	"height":document.documentElement.clientHeight
+			 });
 /*   -- resize screen --*/
 
 												//Event Listeners
 
 		/*  -- On click event handler - change source videoplayer--*/
-	$(".child").on("click",".chan",  function(){
+	$(".left").on("click",".chan",  function(){
 		console.log("clicked");
 		var playlist = $edge + $(this).attr('data-id') + '.m3u8';
-		$("#iPlayer").attr("src", playlist);
+		$play.load(playlist);
 	});
 
-	$(".child").on("focus",".chan", function(){
-		this.addEventListener("keydown", function(event){
-			if(event.keyCode == "0x0D") alert ("49 is pressed when on focus");
-		})
-	})
 
-//	-- increase switch channel speed
-// $(document).ready(function(){
-// 	var chanels = document.getElementsByClassName('chan');
-// 	console.log("channels.length:" + chanels.length);
-// 	for (i = 0 ; i < chanels.length ; i ++){
-// 		chanels[i].addEventListener("click", function(){
-// 			var playlist = $edge + $(this).attr('data-id') + '.m3u8';
-// 			console.log($edge + $(this).attr('data-id') + '.m3u8');
-// 			// document.getElementById('playlist').src = playlist;
-// 			$("#iPlayer").attr("src", playlist);
-// 		}) 	
-// 	}
-// })
+
+
 		/* -- On click show/hide channel panel --*/
 	$("#iPlayer").on('click',function(){
-		var visible = $(".child").css("visibility");
+		var visible = $(".left").css("visibility");
 		//$(".child").css("visibility","visible");
 		if(visible=="hidden"){
-			$(".child").css('visibility','visible');
+			$(".left").css('visibility','visible');
 		} else {
-			$(".child").css('visibility','hidden');
+			$(".left").css('visibility','hidden');
 		}
 	})
 
 	/* -- -react to the left/right key  to hide/show channel menu ---*/
 	document.addEventListener("keydown", function(event){
 		//           show / hide menu
-		if(event.keyCode == "0x25") $(".child").css('visibility','hidden');
-		if(event.keyCode == "0x27") $(".child").css('visibility', 'visible');
+		if(event.keyCode == "0x25") $(".left").css('visibility','hidden');
+		if(event.keyCode == "0x27") $(".left").css('visibility', 'visible');
 		//for keyboard down
 		if(event.keyCode == "40") document.getElementsByClassName('chan')[0].focus();
 		//for control panel
