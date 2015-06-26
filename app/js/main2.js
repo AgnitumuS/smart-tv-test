e94f8bd82ffd31000ecc3650c739fd277b8a063f
v.sashyn <v.sashyn@lanet.ua>
2015-06-26 15:56:43 +0300

    lgottwebos: MVC for epg

--- app/js/main2.js
+++ app/js/main2.js
@@ -105,31 +105,34 @@ function Model(title) {
 	}
 }
 
-var Chans = new Model('Chans');
-	Chans.title = 'Chans';
-	Chans.elem = $('#chans');
-	Chans.selectedIndex = -1;
-	Chans.all = [];
-	Chans.currentList = [];
-	Chans.rating = [];
-	Chans.cable = [];
-	Chans.init = function(){
-		var _this = this;
-		$.getJSON(App.api.main + "list.json", function(res){
-			Chans.all = res.list.slice();
-			App.api.img = res.img;
-			res.list.forEach(function(cur, indx){
-				Chans.cable.push(cur.id);
+var Chans = (function () {
+	function M (){
+		this.title = 'Chans';
+		this.selectedIndex = -1;
+		this.all = [];
+		this.currentList = [];
+		this.rating = [];
+		this.cable = [];
+		this.init = function(){
+			var _this = this;
+			$.getJSON(App.api.main + "list.json", function(res){
+				this.all = res.list.slice();
+				App.api.img = res.img;
+				res.list.forEach(function(cur, indx){
+					this.cable.push(cur.id);
+				})
+				this.currentList = this.all; 
+				PubSub.publish(_this.title + '/init');
+				PubSub.publish(_this.title + '/changed');
 			})
-			Chans.currentList = Chans.all; 
-			PubSub.publish(_this.title + '/init');
-			PubSub.publish(_this.title + '/changed');
-		})
-		$.getJSON(App.api.main + App.api.rating, function(res){
-			this.rating = res;
-		})
-	};
+			$.getJSON(App.api.main + App.api.rating, function(res){
+				this.rating = res;
+			})
+		};
 
+	/*
+	* Sort channels by rating or by cable
+	*/
 	Chans.sort = function(type){
  		var arr;
  		if (type === "rating"){
@@ -145,6 +148,142 @@ var Chans = new Model('Chans');
  			}
  		})
  	}
+
+ 	Chans.handleEvent = function(topic){
+ 		var _this = this;
+ 		switch(topic){
+ 			case RootSubMenu.title + '/changeSelectedIndex':
+ 				var filter = function(ind){
+ 					console.log(ind);
+ 					switch (ind) {
+ 						case 0:
+ 							_this.set('currentList', _this.all, function(){
+ 								PubSub.publish(_this.title + '/changeCurrentList');
+ 							})
+ 						break;
+
+ 						case 1:
+ 							_this.set('currentList', [], function(){
+ 								PubSub.publish(_this.title + '/changeCurrentList');
+ 							}) 
+ 						break;
+ 						
+ 						case 2: 
+ 						break;
+ 						
+ 						default : 
+ 							console.log('default in filter');
+ 						break;
+ 					}
+ 				}
+
+ 				filter( RootSubMenu.getSelectedIndex() );
+
+ 			break;
+ 		}
+ 	}
+ 	Chans.getCur = function(){
+ 		return this.currentList[ this.getSelectedIndex() ];
+ 	}
+	}
+
+	M.prototype = new Model('Chans');
+	var M = new M();
+
+	function V () {
+		// body...
+	}
+
+	function C (m,v,c) {
+
+	}
+
+
+
+})();
+
+
+var Chans = new Model('Chans');
+	// Chans.title = 'Chans';
+	// Chans.elem = $('#chans');
+	// Chans.selectedIndex = -1;
+	// Chans.all = [];
+	// Chans.currentList = [];
+	// Chans.rating = [];
+	// Chans.cable = [];
+	// Chans.init = function(){
+	// 	var _this = this;
+	// 	$.getJSON(App.api.main + "list.json", function(res){
+	// 		Chans.all = res.list.slice();
+	// 		App.api.img = res.img;
+	// 		res.list.forEach(function(cur, indx){
+	// 			Chans.cable.push(cur.id);
+	// 		})
+	// 		Chans.currentList = Chans.all; 
+	// 		PubSub.publish(_this.title + '/init');
+	// 		PubSub.publish(_this.title + '/changed');
+	// 	})
+	// 	$.getJSON(App.api.main + App.api.rating, function(res){
+	// 		this.rating = res;
+	// 	})
+	// };
+
+	// /*
+	// * Sort channels by rating or by cable
+	// */
+	// Chans.sort = function(type){
+ // 		var arr;
+ // 		if (type === "rating"){
+ // 			arr = $channels.rating;
+ // 		} else if (type == "cable"){
+ // 			arr = $channels.cable;
+ // 		}
+ // 		this.chans.sort(function( a , b ){
+ // 			if ( arr.indexOf(a) && arr.indexOf(b) ) {
+ // 				return arr.indexOf(a.id) - arr.indexOf(b.id);
+ // 			} else {
+ // 				return 0;
+ // 			}
+ // 		})
+ // 	}
+
+ // 	Chans.handleEvent = function(topic){
+ // 		var _this = this;
+ // 		switch(topic){
+ // 			case RootSubMenu.title + '/changeSelectedIndex':
+ // 				var filter = function(ind){
+ // 					console.log(ind);
+ // 					switch (ind) {
+ // 						case 0:
+ // 							_this.set('currentList', _this.all, function(){
+ // 								PubSub.publish(_this.title + '/changeCurrentList');
+ // 							})
+ // 						break;
+
+ // 						case 1:
+ // 							_this.set('currentList', [], function(){
+ // 								PubSub.publish(_this.title + '/changeCurrentList');
+ // 							}) 
+ // 						break;
+ 						
+ // 						case 2: 
+ // 						break;
+ 						
+ // 						default : 
+ // 							console.log('default in filter');
+ // 						break;
+ // 					}
+ // 				}
+
+ // 				filter( RootSubMenu.getSelectedIndex() );
+
+ // 			break;
+ // 		}
+ // 	}
+ // 	Chans.getCur = function(){
+ // 		return this.currentList[ this.getSelectedIndex() ];
+ // 	}
+
  	
 function RootMenuModel (argument){
 	this.title = 'RootMenu';
@@ -152,6 +291,9 @@ function RootMenuModel (argument){
 	this.all = [
 			'channels',
 			'programs',
+			'tags',
+			'genres',
+			'search',
 			'settings',
 			'help'
 		];
@@ -169,6 +311,7 @@ function RootMenuModel (argument){
 		return this.currentList[this.selectedIndex];
 	}
 }
+
 RootMenuModel.prototype = new Model();
 var RootMenu = new RootMenuModel();
 
@@ -178,7 +321,8 @@ function RootSubMenuModel (){
 	this.all = {
 		//look like in all in root menu
 		'0': ['all', 'selected', 'top' ],
-		'1': ['bookmarks', 'top', 'tags', 'genres']
+		'1': ['bookmarks', 'top', 'tags', 'genres'],
+		'2': ['language', 'smartCrop', 'sortChannels']
 	};
 	this.currentList = [],
 	this.changeCurrentList = function(){
@@ -206,38 +350,10 @@ function RootSubMenuModel (){
 RootSubMenuModel.prototype = new Model();
 var RootSubMenu = new RootSubMenuModel();
 
+PubSub.subscribe(RootSubMenu.title + '/changeSelectedIndex', Chans);
 PubSub.subscribe(RootMenu.title + '/changeSelectedIndex', RootSubMenu);
 PubSub.subscribe(RootMenu.title + '/changeCurrentList', RootSubMenu);
 
-
-function BrowseLaneModel (argument) {
-	this.title = 'BrowseLane';
-	this.selectedIndex = 0;
-	this.all = [];
-	this.currentList = [];
-
-	this.init = function(){
-		// Genres.init();
-		// var _this = this;
-	}
-
-	this.handleEvent = function(topic){
-		switch (topic) {
-			case Genres.title + '/init':
-				this.set('all', Genres.all);
-				this.set('currentList', this.all, function(){
-				PubSub.publish(this.title + '/init');
-				PubSub.publish(this.title + '/changed');
-			});
-			break;
-			default:
-				console.log("Observer " + BrowseLane.title +" was subscribed for this topic, but there is no processing");
-			break;
-		}
-	}
-}
-BrowseLaneModel.prototype = new Model();
-var BrowseLane = new BrowseLaneModel();
 
 var Genres = new Model('Genres');
 	Genres.selectedIndex = -1;
@@ -255,18 +371,193 @@ var Genres = new Model('Genres');
 		Genres.all.forEach(function(cur, ind){
 			html += '<span class="genre cat">' + cur[0] +'</span>';
 		})
-		$("#browseLane").html(html);
+		// $("#browseLane").html(html);
 	}
 
-// var Epg = new Model('Epg');
-// 	Epg.epgNowAll = {},
-// 	Epg.getEpgNowAll = function(){
-// 		$.getJSON(App.api.main + 'epg/now', function(res){
-// 			this.epgNowAll = res;
-// 			PubSub.publish(this.name + '/changed');
-// 		})
-// 	}
 
+		//NavigateController
+
+function NavigateController (model, view, contr) {
+	this.M = model;
+	this.V = view;
+	this.C = contr;
+	this.spotlight = function(){
+		this.V.spotlight(this.M.selectedIndex);
+	}
+	this.UP =  function(){
+		if ( this.M.hasElem (this.M.getSelectedIndex() - this.grid.x) )	{
+			this.M.setSelectedIndex ( this.M.getSelectedIndex() - this.grid.x );
+		} else {
+			// switch to upNeighbor
+				bwController.changeView('UP');
+		}
+	},
+
+	this.RIGHT = function(){
+		if( (this.M.getSelectedIndex() +  1)  % this.grid.x  === 0){
+			// switch to rightNeighbor
+				bwController.changeView('RIGHT');
+		} else {
+			if( M.hasElem( M.getSelectedIndex() + 1) ){
+				M.setSelectedIndex ( M.getSelectedIndex() + 1);
+			}
+		}
+	},
+
+	this.DOWN = function(){
+		
+		if ( this.M.hasElem ( this.M.getSelectedIndex() + this.grid.x  ) )	{
+				this.M.setSelectedIndex ( this.M.getSelectedIndex() + this.grid.x);
+				// this.spotlight();
+			// if( this.focusedView.scrollDown ){
+			// 	this.focusedView.scrollDown();
+			// }
+		} else {
+				bwController.changeView('DOWN');
+		}
+
+	},
+
+	this.LEFT = function(){
+		if( (( M.getSelectedIndex() -  1)  % this.grid.x)  === 0){
+			bwController.changeView('LEFT');
+		} else {
+			//switch to left element in matrix
+		}
+		
+	}
+}
+
+
+var Epg = (function (argument) {
+	function M (title) {
+		this.title = title;
+		this.epgNowAll = {};
+		this.selectedIndex = 0;
+		this.curDay = [];
+		this.currentList = [];
+		this.now = {};
+		this.getEpgNowAll = function(){
+			this._this = this;
+			$.getJSON(App.api.main + 'epg/now', function(res){
+				_this.epgNowAll = res;
+				PubSub.publish(_this.name + '/changed');
+			})
+		};
+		this.findNow = function(){
+			var now = Math.floor(new Date().getTime() / 1000);
+			for (var entity = 0; this.curDay.length ; entity ++ ){
+				if( this.curDay[entity].stop > now ){
+					this.now.obj = this.curDay[entity];
+					this.now.ind = entity;
+					this.set('selectedIndex', entity, function(){
+						PubSub.publish(this.title + '/changeSelectedIndex');
+					})
+					break;
+				}
+			}
+			return this.now.ind;
+		}
+		//must set selected index to now
+		this.getCurDay = function(){
+			var _this = this;
+			return $.getJSON(App.api.main + 'epg/' + Chans.getCur().id + '/day', function(res){
+				_this.curDay = res;
+				_this.currentList= _this.curDay;
+			})
+		}
+	}
+	M.prototype = new Model('Epg');
+	var M = new M('Epg');
+	
+	function V () {
+		this.spotlight = function(ind){
+			$('#fullepg .epgentity').removeClass("spotlight");
+			$('.epgentity[tabindex=' + ind +']').addClass('spotlight');
+		}
+		this.render = function(arr, now){
+			var html = '';
+			arr.forEach(function(cur, ind){
+				html += '<span class=epgentity tabindex=' +ind+ '>' + cur.title  + '</span>';
+			})
+			$('#fullepg').html(html);
+		}
+	}
+	var V = new V();
+
+	function C () {
+		this.grid = { x : 1, y : 10 },
+		this.title = 'EpgController';
+		this.start = function(){
+			var _this = this;
+			M.getCurDay()
+				.done(function(){
+					M.findNow();
+					V.render(M.curDay);
+					_this.spotlight();
+				})
+		}
+		this.handleEvent = function(topic){
+			switch (topic){
+				case Chans.title + '/changeSelectedIndex':
+					this.start();
+				break;
+				case M.title + '/changeSelectedIndex':
+					this.spotlight(M.getSelectedIndex());
+				break;
+				default:
+					console.log('Observer ' + this.title + ' was subscribed, but there are no realization');
+				break;
+			}
+		}	
+	}
+
+	// EpgController.prototype = bwController;
+	C.prototype = new NavigateController(M, V, C);
+	var C = new C();
+	PubSub.subscribe(Chans.title + '/changeSelectedIndex', C);
+	PubSub.subscribe(M.title + '/changeSelectedIndex', C);
+
+	return {
+		M : M,
+		V : V,
+		C : C
+	}
+})();
+
+
+function ProgramInfoModel () {
+	this.current = {}	
+}
+
+ProgramInfoModel.prototype = new Model();
+var programInfoModel = new ProgramInfoModel();
+
+function ProgramInfoView () {
+	this.render = function(item){
+		var html = '';
+		html = '<span>' + item.text + '</span';
+		$('#programinfo').html(html);
+	}
+}
+var programInfoView = new ProgramInfoView();
+
+function ProgramInfoController () {
+	this.title = 'ProgramInfoController';
+	this.handleEvent = function(topic){
+		switch (topic){
+			case Epg.M.title + '/changeSelectedIndex':
+				programInfoView.render(Epg.M.now.obj)
+			break;
+			default:
+				console.log('Observer ' + this.title + ' was subscribed, but there are no realization');
+			break;
+		}
+	}
+}
+
+var programInfoController = new ProgramInfoController();
+PubSub.subscribe(Epg.M.title + '/changeSelectedIndex', programInfoController);
 
 /* Observers (Views)*/
 function Observer(){
@@ -306,9 +597,11 @@ function RootSubMenuView(){
 	this.grid = { x : 1, y : this._model.all.length }
 	this.rebuildList = function(){
 		var html = '';
+		if(this._model.currentList){
 			this._model.currentList.forEach(function(cur, ind){
 				html += '<span class="subcat"' + 'tabindex='+ ind +  '>' + cur +'</span>';
 			})
+		}
 			$('#rootSubMenu').html(html);	
 	}
 	this.handleEvent = function(topic){
@@ -347,14 +640,16 @@ function ChansView (argument) {
 	}
 	this.handleEvent = function(topic){
 		switch (topic){
-			case this._model.title + '/changeSelectedIndex' :
-				$('.chan').removeClass("spotlight");
-				$('.chan[tabindex=' + this._model.getSelectedIndex() + ']').addClass("spotlight");
-			break;
+			// case this._model.title + '/changeSelectedIndex' :
+			// 	$('.chan').removeClass("spotlight");
+			// 	$('.chan[tabindex=' + this._model.getSelectedIndex() + ']').addClass("spotlight");
+			// break;
 			case this._model.title + '/init':
 				this.rebuildList("-1");
 			break;
+			case this._model.title + '/changeCurrentList':
 			case this._model.title + '/changed' :
+			//must be category
 				this.rebuildList();
 			break;
 			default:
@@ -378,73 +673,21 @@ function ChansView (argument) {
 	}
 }
 
-
-function BrowseLaneView (argument) {
-	var _this = this;
-	this.elem = $('#browseLane')[0];
-	this.childElem = '.cat';
-	this._model = BrowseLane,
-	// this.getSelectedIndex = function(){
-	// 	return this._model.getSelectedIndex();
-	// }
-	//need current model list
-	this.grid = { x : 1, y : this._model.all.length }
-	this.scrollDown = function(){
-		var step = $(_this.elem).children(":first").width();
-		var cur = $(_this.elem).scrollTop();
-		$(_this.elem).scrollTop( cur + step);	 
-	}
-	this.scrollTop = function(){
-		var step = $(_this.elem).children(":first").width();
-		var cur = $(_this.elem).scrollTop();
-		$(_this.elem).scrollTop( cur - step);	 
-	}
-	this.rebuildList = function(){
-		var list, html;
-
-		list = this._model.all;
-		// console.log(list);
-
-		html = '';
-		
-		list.forEach(function(cur, ind){
-			html += '<span class="cat"' + 'tabindex='+ ind +  '>' + cur[0] +'</span>';
-		})
-		$("#browseLane").html(html);
-	}
-	this.handleEvent = function(topic){
-		switch (topic){
-			case this._model.title + '/changed':
-				this.rebuildList();
-			break;
-			case this._model.title + '/changeSelectedIndex':
-				$('.cat').removeClass("spotlight");
-				$('.cat[tabindex=' + this._model.getSelectedIndex() + ']').addClass("spotlight");
-			break;
-			default:
-				console.log("Observer was subscribed for this topic, but there is no processing : " + topic);
-			break;
-
-		}
-	}
-}
-
 var chansView = new ChansView();
-var browseLaneView = new BrowseLaneView();
+// var browseLaneView = new BrowseLaneView();
 
 
 PubSub.subscribe(Chans.title + '/changeSelectedIndex', chansView);
 PubSub.subscribe(Chans.title + '/changed', chansView);
-PubSub.subscribe(Genres.title + '/init', BrowseLane);
-PubSub.subscribe(BrowseLane.title + '/changed', browseLaneView);
-PubSub.subscribe(BrowseLane.title + '/changeSelectedIndex', browseLaneView);
+// PubSub.subscribe(Genres.title + '/init', BrowseLane);
+// PubSub.subscribe(BrowseLane.title + '/changed', browseLaneView);
+// PubSub.subscribe(BrowseLane.title + '/changeSelectedIndex', browseLaneView);
 
 chansView.leftNeighbor = rootSubMenuView;
 // browseLaneView.rightNeighbor = chansView;
 rootSubMenuView.rightNeighbor = chansView;
 rootSubMenuView.leftNeighbor = rootMenuView;
 rootMenuView.rightNeighbor = rootSubMenuView;
-
 	/*Controllers*/
 function Controller (argument) {
 	// body...
@@ -487,15 +730,33 @@ PubSub.subscribe(Chans.title + '/init', loadingController);
 PubSub.subscribe(Genres.title + '/init', loadingController);
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
 function BrowseViewController (argument) {
 	this.focusedView = {},
 	this._model = {};
+	this.activeController = {};
 
 	this.init = function(){
-		this.focusedView = chansView;
-		this._model = this.focusedView._model;
-		this._model.setSelectedIndex(this._model.getSelectedIndex() === -1 ? 0 : this._model.getSelectedIndex());
-		this.spotlight();
+		// kostil
+		Chans.setSelectedIndex(Chans.getSelectedIndex() === -1 ? 0 : Chans.getSelectedIndex());
+
+		// this.focusedView = chansView;
+		// this._model = this.focusedView._model;
+		// this._model.setSelectedIndex(this._model.getSelectedIndex() === -1 ? 0 : this._model.getSelectedIndex());
+		// this.spotlight();
+		this.activeController = Epg.C;
 	}
 	this.spotlight = function(){
 		$(this.focusedView.childElem).removeClass('spotlight');
@@ -506,6 +767,7 @@ function BrowseViewController (argument) {
 		// $( this.focusedView.childElem +  '[tabindex=' + this._model.getSelectedIndex() + ']').removeClass("spotlight");
 		var witch = {};
 		if (orient) {
+
 			switch (orient){
 				case 'UP':
 					witch = this.focusedView.upNeighbor;
@@ -524,6 +786,7 @@ function BrowseViewController (argument) {
 				break;
 			}
 			if (witch) {
+				$(this.focusedView.childElem).removeClass('spotlight');
 				this.focusedView = witch;
 				this._model = this.focusedView._model;
 				console.log('changeView to : ');
@@ -541,18 +804,19 @@ function BrowseViewController (argument) {
 	}
 
 	this.UP =  function(){
-		console.log('UP evnt in BrowseViewController');
+		// console.log('UP evnt in BrowseViewController');
 
-		if ( this._model.hasElem (this._model.getSelectedIndex() - this.focusedView.grid.x) )	{
-			this._model.setSelectedIndex (  this._model.getSelectedIndex() - this.focusedView.grid.x );
-			this.spotlight();
-			if(this.focusedView.scrollTop){
-				this.focusedView.scrollTop();
-			}
-		} else {
-			// switch to upNeighbor
-				this.changeView('UP');
-		}
+		// if ( this._model.hasElem (this._model.getSelectedIndex() - this.focusedView.grid.x) )	{
+		// 	this._model.setSelectedIndex (  this._model.getSelectedIndex() - this.focusedView.grid.x );
+		// 	this.spotlight();
+		// 	if(this.focusedView.scrollTop){
+		// 		this.focusedView.scrollTop();
+		// 	}
+		// } else {
+		// 	// switch to upNeighbor
+		// 		this.changeView('UP');
+		// }\
+		this.activeController.UP();
 	},
 
 	this.RIGHT = function(){
@@ -568,16 +832,16 @@ function BrowseViewController (argument) {
 
 	this.DOWN = function(){
 		
-		if ( this._model.hasElem (this._model.getSelectedIndex() + this.focusedView.grid.x  ) )	{
-				this._model.setSelectedIndex ( this._model.getSelectedIndex() + this.focusedView.grid.x);
-				this.spotlight();
-			if(this.focusedView.scrollDown){
-				this.focusedView.scrollDown();
-			}
-		} else {
-				this.changeView('DOWN');
-		}
-
+		// if ( this._model.hasElem (this._model.getSelectedIndex() + this.focusedView.grid.x  ) )	{
+		// 		this._model.setSelectedIndex ( this._model.getSelectedIndex() + this.focusedView.grid.x);
+		// 		this.spotlight();
+		// 	if(this.focusedView.scrollDown){
+		// 		this.focusedView.scrollDown();
+		// 	}
+		// } else {
+		// 		this.changeView('DOWN');
+		// }
+		this.activeController.DOWN();
 	},
 
 	this.LEFT = function(){
@@ -633,9 +897,42 @@ App.start();
 
 
 
+var Player = (function () {
+	// body...
+	function method(){
 
+	}
+	return {
+		method : method
+	}
+})();
 
+var Store = (function () {
+	// body...
+	var prefix = '_db';
+	var store =  {};
 
+	if(typeof(Storage) !== undefined){
+		store = window.localStorage;
+	} else {
+		throw new 'LocalStorage doesn\'t support!'
+	}
+
+	function set(key , val){
+		store.setItem(key, val);
+	}
+	function get(key){
+		return store.getItem(key); 
+	}
+	function remove(key){
+		return store.removeItem(key);
+	}
+	return {
+		set : set,
+		get : get
+	}
+
+})();
 
 
 
