08656431c02f6dccbe4065cb198abc5f3dbf4ae9
a.shulgin <a.shulgin@lanet.ua>
2016-02-15 17:52:29 +0200

    meging

---
+++ js/social-m.js
@@ -0,0 +1,159 @@
+lanet_tv.Social = (function () {
+    var instance;
+
+    function init() {
+        var body = document.getElementsByTagName('body')[0],
+            social = document.createElement('div'),
+            auth = document.createElement('div'),
+            main = document.createElement('div'),
+            welcome = document.createElement('div'),
+            hint = document.createElement('div'),
+            reset = document.createElement('div'),
+            storage = lanet_tv.Storage.getInstance(),
+            welcome_words = ['Hello,', 'Howdy,', 'Welcome,', 'Bonjour,', 'Buenos dias,', 'Shalom,'],
+            userpic, key, requests = [], pin, pin_timeout, pin_check_url, open = false,
+            onAuthUpdate = function (userpic, key) { },
+            createElement = function () {
+                social.id = 'social';
+                auth.className = 'auth';
+                main.className = 'main';
+                reset.className = 'reset';
+                welcome.className = 'welcome';
+                hint.innerHTML = '<a target="_blank" href="https://auth.lanet.tv/test">https://auth.lanet.tv/test</a>';
+                hint.className = 'hint';
+                reset.innerHTML = 'OK - Log out';
+                userpic = '';
+                key = null;
+
+                var facebook = document.createElement('button');
+                facebook.className = 'social facebook';
+                var facebook_text = document.createElement('span');
+                facebook_text.innerHTML = 'Facebook';
+                facebook.appendChild(facebook_text);
+
+                var google = document.createElement('button');
+                google.className = 'social google';
+                var google_text = document.createElement('span');
+                google_text.innerHTML = 'Google';
+                google.appendChild(google_text);
+
+                var vk = document.createElement('button');
+                vk.className = 'social vk';
+                var vk_text = document.createElement('span');
+                vk_text.innerHTML = 'Вконтакте';
+                vk.appendChild(vk_text);
+
+                var lanet = document.createElement('button');
+                lanet.className = 'social lanet';
+                var lanet_text = document.createElement('span');
+                lanet_text.innerHTML = 'Ланет';
+                lanet.appendChild(lanet_text);
+
+                main.appendChild(facebook);
+                main.appendChild(google);
+                main.appendChild(vk);
+                main.appendChild(lanet);
+
+                auth.appendChild(main);
+                social.appendChild(auth);
+                //social.appendChild(reset);
+                reset.addEventListener('click', resetAuth);
+                Helpers.hideNode(reset);
+                return social;
+            },
+            resetAuth = function () {
+                Helpers.hideNode(reset);
+                storage.set('token', '');
+                storage.set('key', '');
+                pin = false;
+                pin_check_url = false;
+                userpic = '';
+                key = null;
+                onAuthUpdate(userpic, key);
+                updateAuth();
+            },
+            saveAuth = function (data) {
+                //Helpers.showNode(reset);
+                //Helpers.removeChildren(main);
+                storage.set('token', data['token']);
+                storage.set('key', data['key']);
+                //welcome.innerHTML = welcome_words[Math.floor(Math.random() * welcome_words.length)];
+                //main.appendChild(welcome);
+                //main.innerHTML += data['name'];
+                userpic = data['image'];
+                key = data['key'];
+                onAuthUpdate(userpic, key);
+            },
+            checkPin = function (url) {
+                requests.push(Helpers.getJSON(url, function (data) {
+                    data['status'] == 'ok' ? saveAuth(data) : resetAuth();
+                }, function (error) {
+                    if (error.status != 0 && open)
+                        updateAuth();
+                }));
+            },
+            refreshPin = function () {
+                requests.push(Helpers.getJSON('https://auth.lanet.tv/login/pin', function (data) {
+                    pin = data['code'];
+                    pin_check_url = data['status'];
+                    pin_timeout = setTimeout(function () {
+                        pin = false;
+                        pin_check_url = false;
+                        updateAuth();
+                    }, data['expire'] * 1000);
+                    main.innerHTML = data['code'];
+                    main.appendChild(hint);
+                    updateAuth();
+                }));
+            },
+            checkToken = function (token) {
+                requests.push(Helpers.getJSON('https://auth.lanet.tv/token/' + token, function (data) {
+                    data['status'] == 'ok' ? saveAuth(data) : resetAuth();
+                }, function (error) {
+                    if (error.status != 0)
+                        resetAuth()
+                }));
+            },
+            updateAuth = function () {
+                for (var r in requests) { if (requests.hasOwnProperty(r)) requests[r].abort(); }
+                requests = [];
+                if (storage.get('key').length == 0) {
+                    if (storage.get('token').length > 0) {
+                        checkToken(storage.get('token'));
+                    } else if (open) {
+                        pin_check_url ? checkPin(pin_check_url) : refreshPin();
+                    }
+                }
+            };
+        body.appendChild(createElement());
+        storage.set('key', '');
+        updateAuth();
+        return {
+            show: function () {
+                open = true;
+                Helpers.showNode(social);
+                updateAuth();
+            },
+            hide: function () {
+                Helpers.hideNode(social);
+                open = false;
+            },
+            resetAuth: resetAuth,
+            setAuthUpdateFunction: function (func) {
+                onAuthUpdate = func;
+                if (userpic && key) onAuthUpdate(userpic, key)
+            },
+            getKey: function() {
+                return key;
+            }
+        };
+    }
+
+    return {
+        getInstance: function () {
+            if (!instance)
+                instance = init();
+            return instance;
+        }
+    }
+})();
