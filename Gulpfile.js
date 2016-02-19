9d48dc3b3483ac77425022df5948dbbc19a98a73
andrew-shulgin <andrewshulginua@gmail.com>
2016-02-19 05:44:03 +0200

    Smart TV Gulp

--- Gulpfile.js
+++ Gulpfile.js
@@ -80,6 +80,78 @@ gulp.task('ios', function () {
         .pipe(gulp.dest('build/ios/assets'));
 });
 
+gulp.task('smart', function () {
+    var js_prefix = "src/js",
+        css_prefix = "src/css",
+        js_files = [
+            "extend.js",
+            "helpers.js",
+            "preloader.js",
+            "time.js",
+            "inobounce.js",
+            "app.js",
+            "api.js",
+            "storage.js",
+            "input.js",
+            "platform/pc.js",
+            "platform/lg.js",
+            "platform/dune.js",
+            "platform/samsung.js",
+            "channels.js",
+            "app_bar.js",
+            "control_bar.js",
+            "menu.js",
+            "auth.js",
+            "player.js",
+            "remote.js",
+            "main.js"
+        ],
+        css_files = [
+            "fonts.css",
+            "icons.css",
+            "global.css",
+            "menu.css",
+            "auth.css",
+            "app_bar.css",
+            "control_bar.css"
+        ];
+    js_files = js_files.map(function (file) {
+        return js_prefix + "/" + file;
+    });
+    css_files = css_files.map(function (file) {
+        return css_prefix + "/" + file;
+    });
+    gulp.src(js_files)
+        .pipe(uglify())
+        .pipe(concat('app.min.js'))
+        .pipe(gulp.dest('build/smart'));
+    gulp.src(css_files)
+        .pipe(replace(/\.\.\//g, ''))
+        .pipe(cleancss())
+        .pipe(concat('app.min.css'))
+        .pipe(gulp.dest('build/smart'));
+    gulp.src("./src/template.html")
+        .pipe(template({
+            title: "Ланет TV для Smart TV",
+            css: "app.min.css",
+            js: "app.min.js"
+        }))
+        .pipe(htmlmin({
+            collapseWhitespace: true
+        }))
+        .pipe(rename("index.html"))
+        .pipe(gulp.dest('build/smart'));
+    gulp.src("src/favicon.ico")
+        .pipe(gulp.dest('build/smart'));
+    gulp.src("src/assets/**/*")
+        .pipe(imagemin({
+            progressive: true,
+            svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+        }))
+        .pipe(gulp.dest('build/smart/assets'));
+});
+
 gulp.task('deploy-ios', function () {
     var config = require('./ftp.json'),
         conn = ftp.create({
